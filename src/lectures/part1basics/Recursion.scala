ae1b3a3426add76f4a931fa599a2c3101ae4a462
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-02 20:08:59 +0200

    Video 6 done

--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -18,7 +18,7 @@ object Recursion extends App {
   def anotherFactorial(n: Int): BigInt = {
     def factHelper(x: Int, accumulator: BigInt): BigInt = {
       if (x <= 1) accumulator
-      else factHelper(x - 1, x * accumulator)
+      else factHelper(x - 1, x * accumulator) // tail recursion
     }
 
     factHelper(n, 1)
@@ -38,6 +38,66 @@ object Recursion extends App {
      10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
    */
 
-  print(anotherFactorial(5000))
+  //print(anotherFactorial(5000))
+
+  // when you need loops, use tail recursion
+
+  def concatenateString(aString: String, n: Int): String = {
+    def concatenateHelper(aString: String, actualResult: String, n: Int): String = {
+      if (n == 1) actualResult
+      else concatenateHelper(aString, actualResult + aString, n - 1)
+    }
+    concatenateHelper(aString, aString, n)
+  }
+
+  println(concatenateString("hello",4))
+  println(concatenateString("one hello",1))
+
+  def isPrime(n: Int): Boolean = {
+    def isPrime(n: Int, divisor: Int, divisors: Int): Boolean = {
+      if (divisors == 2) false
+      else {
+        if (divisor > n/2) true
+        else {
+          if (n % divisor == 0) isPrime(n, divisor + 1, divisors + 1)
+          else isPrime(n, divisor + 1, divisors)
+        }
+      }
+    }
+    isPrime(n, 1, 0)
+  }
+
+  println(isPrime(3))
+  println(isPrime(5))
+  println(isPrime(6))
+  println(isPrime(7))
+  println(isPrime(13))
+  println(isPrime(15))
+  println(isPrime(83))
+  println(isPrime(84))
+
+  def fibonacci(n : Int): Int = {
+    if (n <= 2) 1
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  def fibonacci2(n: Int): Int = {
+    def fibonacciHelper(n: Int, actual1: Int, actual2: Int): Int = {
+      if (n < 2) actual1 + actual2
+      else fibonacciHelper(n - 1, actual2, actual1 + actual2)
+    }
+    fibonacciHelper(n - 1, 0, 1)
+  }
+
+  println(fibonacci(9))
+  println(fibonacci2(9))
+  println(fibonacci(4))
+  println(fibonacci2(4))
+  println(fibonacci(15))
+  println(fibonacci2(15))
+
+
+
+
 
 }
