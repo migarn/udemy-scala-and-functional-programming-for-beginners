be4c0403bd5653ffdd543d10cba790ab793175b7
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-08 19:38:47 +0200

    Video 12 done

---
+++ src/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,45 @@
+package lectures.part2oop
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favouriteMovie: String) {
+    def likes(movie: String): Boolean = movie == favouriteMovie
+    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck?!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favouriteMovie"
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // infix notation = operator notation (syntactic sugar)
+
+  val tom = new Person("Tom", "Fight Club")
+  println(mary hangOutWith tom)
+  println(mary + tom)
+  println(mary.+(tom))
+
+  println(1 + 2)
+  println(1.+(2))
+
+  // ALL OPERATORS ARE METHODS
+
+  // Prefix notation:
+
+  val x = -1 // equivalentt with 1.unary_-
+  val y = 1.unary_-
+  // unary prefix only works with - + ~ !
+
+  println(!mary)
+  println(mary.unary_!)
+
+  // Postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary())
+
+}
