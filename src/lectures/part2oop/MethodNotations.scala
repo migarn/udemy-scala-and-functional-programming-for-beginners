641748cb40dcf6fb798cf40c1fb9ffa44c86d182
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-09 19:01:26 +0200

    Video 13 done

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,13 +2,18 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favouriteMovie: String) {
+  class Person(val name: String, favouriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favouriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def unary_! : String = s"$name, what the heck?!"
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favouriteMovie"
+    def +(nick: String): Person = new Person(name + " (" + nick + ")", favouriteMovie)
+    def unary_+ : Person = new Person(name, favouriteMovie, age + 1)
+    def learns(s: String): String = name + " learns " + s
+    def learns(): String = learns("Scala")
+    def apply(n: Int): String = name + " watched " + favouriteMovie + " " + n + " times"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -41,5 +46,11 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary())
+
+  println((mary + "the rockstar")())
+  println((+mary).age)
+  println(mary learns "Scala")
+  println(mary learns)
+  println(mary(2))
 
 }
