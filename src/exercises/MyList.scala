ae399d2ed49622c8cbbd9a378443aeb7cf7dbae3
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-16 08:54:01 +0200

    Video 20 in process

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -7,6 +7,12 @@ abstract class MyList[+A] {
   def add[B >: A](element: B): MyList[B]
   def printElements: String
   override def toString: String = "[" + printElements + "]"
+
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  // time 9:00
 }
 
 object Empty extends MyList[Nothing] {
@@ -26,6 +32,14 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
   }
+}
+
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(elem: A): B
 }
 
 object ListTest extends App {
