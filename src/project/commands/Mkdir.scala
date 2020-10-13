5c0c9f56e16de17c4c5a2e9b597fa7594f3d989b
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-13 18:15:23 +0200

    Video 41 done

--- src/project/commands/Mkdir.scala
+++ src/project/commands/Mkdir.scala
@@ -1,12 +1,12 @@
 package project.commands
-import project.files.Directory
+import project.files.{DirEntry, Directory}
 import project.filesystem.State
 
 class Mkdir(name: String) extends Command {
 
   override def apply(state: State): State = {
     val wd = state.wd
-    if (wd.hasENtry(name)) {
+    if (wd.hasEntry(name)) {
       state.setMessage("Entry " + name + " already exists!")
     } else if (name.contains(Directory.SEPARATOR)) {
       state.setMessage(name + " must not containt separators!")
@@ -22,7 +22,21 @@ class Mkdir(name: String) extends Command {
   }
 
   def doMkdir(state: State, str: String): State = {
-    ???
+
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+      }
+    }
+
+    val wd = state.wd
+    val allDirsInPath = wd.getAllFoldersInPath
+    val newDir = Directory.empty(wd.path, name)
+    val newRoot = updateStructure(state.root, allDirsInPath, newDir)
+    val newWd = newRoot.findDescendant(allDirsInPath)
+    State(newRoot, newWd)
   }
 
 }
--- src/project/files/Directory.scala
+++ src/project/files/Directory.scala
@@ -3,7 +3,20 @@ package project.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
-  def hasENtry(name: String):Boolean = ???
+  def hasEntry(name: String):Boolean = ???
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+
+  def asDirectory: Directory = this
 
 }
 
