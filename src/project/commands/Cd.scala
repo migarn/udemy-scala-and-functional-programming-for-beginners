d8249d357054ed6bc9c78978ad2f5b3cb746b6a2
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-17 17:12:06 +0200

    Video 45 done

--- src/project/commands/Cd.scala
+++ src/project/commands/Cd.scala
@@ -7,26 +7,24 @@ import scala.annotation.tailrec
 class Cd(dir: String) extends Command {
 
   override def apply(state: State): State = {
+
     val root = state.root
     val wd = state.wd
+
     val absolutePath =
       if (dir.startsWith(Directory.SEPARATOR)) dir
-      else if (wd.isRoot) wd.path + dir
+      else if(wd.isRoot) wd.path + dir
       else wd.path + Directory.SEPARATOR + dir
 
     val destinationDirectory = doFindEntry(root, absolutePath)
 
-    if (destinationDirectory == null || destinationDirectory.isDirectory)
+    if (destinationDirectory == null || !destinationDirectory.isDirectory)
       state.setMessage(dir + ": no such directory")
     else
       State(root, destinationDirectory.asDirectory)
-
-
-
   }
 
   def doFindEntry(root: Directory, path: String): DirEntry = {
-
     @tailrec
     def findEntryHelper(currentDirectory: Directory, path: List[String]): DirEntry =
       if (path.isEmpty || path.head.isEmpty) currentDirectory
@@ -37,11 +35,21 @@ class Cd(dir: String) extends Command {
         else findEntryHelper(nextDir.asDirectory, path.tail)
       }
 
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      if (path.isEmpty) result
+      else if(".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if("..".equals(path.head)) {
+        if (result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
+
     val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
 
-    findEntryHelper(root, tokens)
+    val newTokens = collapseRelativeTokens(tokens, List())
+
+    if (newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
-
-
-
 }
