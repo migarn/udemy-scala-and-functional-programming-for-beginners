2caff4c37d2c7471496e3c9203e77e39d73e212d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-21 18:29:16 +0200

    Video 48 done

--- src/project/commands/Echo.scala
+++ src/project/commands/Echo.scala
@@ -1,4 +1,5 @@
 package project.commands
+import project.files.{Directory, File}
 import project.filesystem.State
 
 import scala.annotation.tailrec
@@ -23,8 +24,35 @@ class Echo(args: Array[String]) extends Command {
     }
   }
 
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String], contents: String, append: Boolean): Directory = {
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+      if (dirEntry == null)
+        currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+      else if (dirEntry.isDirectory) currentDirectory
+      else
+        if (append) currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+      else currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+    } else {
+      val nextDirectory = currentDirectory.findEntry(path.head).asDirectory
+      val newNextDirectory = getRootAfterEcho(nextDirectory, path.tail, contents, append)
+
+      if (newNextDirectory == nextDirectory) currentDirectory
+      else currentDirectory.replaceEntry(path.head, newNextDirectory)
+    }
+  }
+
   def doEcho(state: State, contents: String, filename: String, append: Boolean): State = {
-    ???
+    if (filename.contains(Directory.SEPARATOR))
+      state.setMessage("Echo: filename must not contains separators")
+    else {
+      val newRoot: Directory = getRootAfterEcho(state.root, state.wd.getAllFoldersInPath :+ filename, contents, append)
+      if (newRoot == state.root)
+        state.setMessage(filename + ": no such file")
+      else
+        State(newRoot, newRoot.findDescendant(state.wd.getAllFoldersInPath))
+    }
   }
 
   def createContent(args: Array[String], topIndex: Int): String = {
